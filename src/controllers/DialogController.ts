ec15d9e9943ee780d0796aca0032237ec7772c40
Alexey Oblomov <alexey.oblomov.1981@gmail.com>
2020-09-03 18:52:40 +0300

    fix Dialog controller

--- src/controllers/DialogController.ts
+++ src/controllers/DialogController.ts
@@ -6,7 +6,7 @@ class DialogController {
     const authorId: string = req.params.id;
 
     DialogModel.find({ author: authorId })
-      .populate('dialogs')
+      .populate(['author', 'partner'])
       .exec(function (err, dialogs) {
         if (err) {
           return res.status(404).json({
